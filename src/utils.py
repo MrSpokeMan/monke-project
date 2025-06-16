ea5cfce5fd4c5e0a6a4baab93c2e5d69d806a036
WojtAcht <wachtelik@gmail.com>
2025-06-16 15:04:16 +0200

    feat: add support for mps

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import torch
+from typing import Literal
+
+
+def get_device() -> Literal["cuda", "mps", "cpu"]:
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    else:
+        return "cpu"
